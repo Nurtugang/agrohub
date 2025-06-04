09463f58204234d7483d35422833612a49a2213f
Nurtugang <nurtugang17@gmail.com>
2025-06-04 12:03:50 +0500

    configs changed compatible for production

--- agrohub/urls.py
+++ agrohub/urls.py
@@ -1,4 +1,3 @@
-"""myproject URL Configuration"""
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
@@ -9,10 +8,12 @@ urlpatterns = [
     path('', include('main_app.urls')),
 ]
 
-if bool(settings.DEBUG):
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 if 'rosetta' in settings.INSTALLED_APPS:
     urlpatterns += [
         re_path(r'^rosetta/', include('rosetta.urls'))
     ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
--- agrohub/wsgi.py
+++ agrohub/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for agrohub project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agrohub.settings')
 
 application = get_wsgi_application()
+
